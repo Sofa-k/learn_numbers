581971bfc71839f2f0fc2820948654e3c87d2fef
ReyBroncas <andriksmen@gmail.com>
2019-10-25 22:49:52 +0300

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import sequences
 diff_list = ((0,20),(21,100),(101,200))
+#x = int(input())-1
+x = 2
+print(sequences.lucky_numbers(diff_list[x]))
+print(sequences.prime_numbers(diff_list[x]))
+print(sequences.ulam_numbers(diff_list[x]))
