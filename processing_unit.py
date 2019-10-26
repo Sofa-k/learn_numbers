0a350c543992170d8d643f31044ecde5450e3685
Mark <valyavka@ucu.edu.ua>
2019-10-27 00:24:22 +0300

    push mycora v master

--- processing_unit.py
+++ processing_unit.py
@@ -1,15 +1,9 @@
 import random
 import sequences
-
-enemy_health = 5
-your_health = 5
-score = 0
-ulam_numbers = [1,2,3]
-prime_numbers = [4,5,6]
-lucky_numbers = [7,8,9]
+from essential import *
+from res import *
 
 def question():
-
     ulam_question = """
     +------------------------------------------------------+
     |                                                      |
@@ -31,22 +25,26 @@ def question():
     |                                                      |
     +------------------------------------------------------+
     """
-    question_list = [(ulam_question,1),(lucky_question,2),(prime_question,3)]
+    question_list = [(ulam_question, 1), (lucky_question, 2), (prime_question, 3)]
     return random.choice(question_list)
+
+
+
 while enemy_health > 0 and your_health > 0:
-    if your_health == 1:
-        print("Its either you or him! Make sure you don't miss this time")    #  <---- Пистолет направляется на юзера
     drawq = question()
     print(drawq[0])
-    x = int(input())
-    if ((x in ulam_numbers) and (drawq[1] == 1)) or\
-       ((x in lucky_numbers) and (drawq[1] == 2)) or\
-       ((x in prime_numbers) and (drawq[1] == 3)):
+    user_input = int(input())
+    if your_health == 1:
+        print("Its either you or him! Make sure you don't miss this time")    #  <---- Пистолет направляется на юзера
+
+    if ((user_input in ulam_numbers) and (drawq[1] == 1)) or \
+            ((user_input in lucky_numbers) and (drawq[1] == 2)) or \
+            ((user_input in prime_numbers) and (drawq[1] == 3)):
 
         enemy_health = enemy_health - 1
-        print("Nice shot!","Enemy health is now: ",enemy_health)
+        print("Nice shot!", "Enemy health is now: ", enemy_health)
     else:
         your_health = your_health - 1
-        print("Ouuch! That hurts","Your healh is now: ",your_health)
+        print("Ouuch! That hurts", "Your healh is now: ", your_health)
 
 
