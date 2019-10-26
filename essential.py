da91099439129ded52489b31e6ecc89a969fc81e
ReyBroncas <andriksmen@gmail.com>
2019-10-26 18:39:33 +0300

    added score

--- essential.py
+++ essential.py
@@ -1,23 +1,16 @@
 import sys
 import res
 def draw(gun,man):
-
-    '''for i in range(0, 16):
-        for j in range(0, 16):
-            code = str(i * 16 + j)
-            sys.stdout.write(u"\u001b[38;5;" + code + "m " + code.ljust(4))
-    print(u"\u001b[0m")
-    sdkmskdjslklj
-    '''
     global_line = []
     for line in range(16):
-        global_line.extend(''.join(gun[line]+(' '*30)+man[line]+'H'))
+        global_line.extend(''.join(gun[line]+(' '*40)+man[line]+'H'))
     global_line = denester(global_line,16,len(global_line)/16)
-    print('---------------------------------------------------------------------------------------')
+    score = 123
+    
+    print('-'*((len(global_line[1])-len(str(score))-2)//2)+' '+str(score)+' '+'-'*((len(global_line[1])-len(str(score))-2)//2))
+
     for each in global_line:
         print(each)
-    print('---------------------------------------------------------------------------------------')
-
 
 def denester(asciistr,height,width):
     '''
