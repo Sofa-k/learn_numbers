3582b439995ad105708c7787156fb7973dbc7f85
ReyBroncas <andriksmen@gmail.com>
2019-10-28 18:41:33 +0200

    merged whiles

--- main.py
+++ main.py
@@ -23,30 +23,23 @@ text_area = qustion_area_maker(qustion_text_1)
 user_x_anim = [user_x_anim_1,user_x_anim_2,user_x_anim_3,user_x_anim_4,user_x_anim_5,user_x_anim_6,user_x_anim_7,user_x_anim_1,user_x_anim_1,user_x_anim_1]
 man_x_anim = [man_x_anim_1,man_x_anim_1,man_x_anim_1,man_x_anim_1,man_x_anim_1,man_x_anim_1,man_x_anim_1,man_x_anim_8,man_x_anim_9,man_x_anim_10]
 
-anim_global_line = []
+anim_x_global_line = []
 i=0
 while i < len(user_x_anim):
     user_x_anim[i] = denester(user_x_anim[i],9,len(user_x_anim[i])/9)
-    i+=1
-i=0
-while i < len(user_x_anim):
     man_x_anim[i] = denester(man_x_anim[i],16,len(man_x_anim[i])/16)
+    anim_x_global_line.append(draw_proccessing(text_area,user_x_anim[i],man_x_anim[i],0,0,0,0,True))
     i+=1
-i=0
-while i < 10:
 
-    anim_global_line.append(draw_proccessing(text_area,user_x_anim[i],man_x_anim[i],0,0,0,0,True))
 
-    i += 1
-
-# line = draw_proccessing(text_area,user_live,cowboy_alive,heart,5,5,134)
-# draw(line[0],line[1])
-# x = 0
-# while x < 10:
-#     draw(line[0],anim_global_line[x])
-#     time.sleep(0.04)
-#     x += 1
-
+line = draw_proccessing(text_area,user_live,cowboy_alive,heart,5,5,134)
+draw(line[0],line[1])
+x = 0
+while x < 10:
+    draw(line[0],anim_x_global_line[x])
+    time.sleep(0.04)
+    x += 1
+'''
 man_life_int = 5
 user_life_int = 5
 
@@ -75,6 +68,4 @@ while man_life_int > 0 and user_life_int > 0:
     else:
         continue
 
-print("While has finis")
-
-
+print("While has finis")'''
